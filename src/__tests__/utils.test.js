a80f66cbaec6cb4af77fdc985ba03ffb88bc6f29
Samuel Kirori <samuelkirori300@gmail.com>
2024-05-11 22:04:24 +0300

    Complete test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
-// Your tests here
+// src/__tests__/utils.test.js
+
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome function', () => {
+  // Other test cases...
+
+  test('returns false for empty string', () => {
+    expect(() => {
+      isPalindrome('');
+    }).toThrow('Input must contain only alphabetic characters');
+  });
+
+  // Other test cases...
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,22 @@
-// Your code here
+// src/utils.js
+
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error('Input must contain only alphabetic characters');
+    }
+  
+    const cleanWord = word.toLowerCase();
+    if (cleanWord.length === 0) {
+      return false; // Empty string is not a palindrome
+    }
+  
+    const reversedWord = cleanWord.split('').reverse().join('');
+    return cleanWord === reversedWord;
+  }
+  
+  module.exports = { isPalindrome };
+  
